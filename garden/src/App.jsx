95113643b71d60bb15e19eedbfd7987800552038
Vilesoni <beautiful_deer9@mail.ru>
2021-05-28 09:10:23 +0300

    Start

---
+++ garden/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <img src="images/art1.jpg"/>
+    </div>
+  );
+};
+export default App;
